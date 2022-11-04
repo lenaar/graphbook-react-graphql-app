1836cb6a09fcf4e5120244514dfdf22dd0dd0c1c
Elena Rakhimova <elenara@kth.se>
2022-11-04 15:36:45 +0100

    fix: add mutation resolver function and schema to posts

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,7 +1,27 @@
+import { faker } from "@faker-js/faker";
+
+let posts = [1, 2].map((id) => ({
+  id,
+  text: faker.lorem.paragraph(),
+  user: { avatar: faker.image.avatar(), username: faker.internet.userName() },
+}));
+
 const resolvers = {
   RootQuery: {
     posts(root, args, context) {
-      return [];
+      return posts;
+    },
+  },
+  RootMutation: {
+    addPost(root, { post, user }, context) {
+      const postObject = {
+        ...post,
+        user,
+        id: posts.length + 1,
+      };
+      posts.push(postObject);
+      logger.log({ level: "info", message: "Post was created" });
+      return postObject;
     },
   },
 };
--- src/server/services/graphql/schema.js
+++ src/server/services/graphql/schema.js
@@ -1,18 +1,40 @@
-const typeDefinitions = '
+const typeDefinitions = `
+
+  type User {
+    avatar: String
+    username: String
+  }
 
   type Post {
     id: Int
     text: String
+    user: User
   }
 
   type RootQuery {
     posts: [Post]
   }
 
-  schema {
-    query: RootQuery
+  input PostInput {
+    text: String!
+  }
+  
+  input UserInput {
+    username: String!
+    avatar: String!
+  }
+  
+  type RootMutation {
+    addPost (
+      post: PostInput!
+      user: UserInput!
+    ): Post
   }
 
-';
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+  `;
 
 export default [typeDefinitions];
