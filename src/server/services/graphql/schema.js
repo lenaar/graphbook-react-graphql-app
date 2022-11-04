6d2400b87e92ad701a5c1f6cc889c4b9a09db4da
Elena Rakhimova <elenara@kth.se>
2022-11-04 14:30:18 +0100

    feat: add schema and resolvers for posts

---
+++ src/server/services/graphql/schema.js
@@ -0,0 +1,18 @@
+const typeDefinitions = '
+
+  type Post {
+    id: Int
+    text: String
+  }
+
+  type RootQuery {
+    posts: [Post]
+  }
+
+  schema {
+    query: RootQuery
+  }
+
+';
+
+export default [typeDefinitions];
