cabb0a3f49f1bc7aab792f9853f426fba78ff645
Elena Rakhimova <elenara@kth.se>
2022-11-05 14:15:06 +0100

    feat: add config to db

---
+++ src/server/config/index.js
@@ -0,0 +1,33 @@
+const root = require("path").join(__dirname, "../../../");
+require("dotenv").config({ path: root + ".env" });
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: "127.0.0.1",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+
+  production: {
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    logging: false,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+};
