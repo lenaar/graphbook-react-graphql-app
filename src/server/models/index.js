60b6a960d4b98ee9b690a7a506f2ebb1453d4303
Elena Rakhimova <elenara@kth.se>
2022-11-06 11:36:45 +0100

    fix: add context fetch by model name

---
+++ src/server/models/index.js
@@ -0,0 +1,31 @@
+import Sequelize from "sequelize";
+
+if (process.env.NODE_ENV === "development") {
+  require("babel-plugin-require-context-hook/register")();
+}
+// collect all models into one index file and export it
+export default (sequelize) => {
+  let db = {};
+  const context = require.context(
+    ".",
+    true,
+    /^\.\/(?!index\.js).*\.js$/,
+    "sync"
+  );
+
+  context
+    .keys()
+    .map(context)
+    .forEach((module) => {
+      const model = module(sequelize, Sequelize);
+      db[model.name] = model;
+    });
+
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+
+  return db;
+};
