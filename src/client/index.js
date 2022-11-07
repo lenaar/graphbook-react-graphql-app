2276dc770e5612c3e5bd26a27fd03d37a109bf80
Elena Rakhimova <elenara@kth.se>
2022-11-07 12:57:35 +0100

    feat: add ApolloProvider to provide graphgl data globalt

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,16 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
+import { ApolloProvider } from "@apollo/client/react";
+
 import App from "./App";
+
+import client from "./apollo";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
