5502a44c5c3fe498d250c6464123dc73bdabb1d0
Elena Rakhimova <elenara@kth.se>
2022-11-07 12:56:44 +0100

    refactor: move feed part out of App to Feed component

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import { faker } from "@faker-js/faker";
+import Feed from "./Feed";
 import "../../assets/css/style.css";
 
 const initialPosts = [1, 2].map((id) => ({
@@ -10,26 +11,6 @@ const initialPosts = [1, 2].map((id) => ({
 }));
 
 const App = () => {
-  const [posts, setPosts] = useState(initialPosts);
-  const [postContent, setPostContent] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const newPost = {
-      id: posts.length + 1,
-      text: postContent,
-      user: {
-        avatar: faker.image.avatar(),
-        username: faker.internet.userName(),
-      },
-    };
-    console.log("postContent", postContent);
-
-    setPosts([newPost, ...posts]);
-
-    setPostContent("");
-  };
   return (
     <div className="container">
       <Helmet>
@@ -40,33 +21,7 @@ const App = () => {
           content="Newsfeed of all fake friends on Graphbook"
         />
       </Helmet>
-      <div className="postForm">
-        <form onSubmit={handleSubmit}>
-          <textarea
-            value={postContent}
-            onChange={(e) => {
-              console.log("change");
-              setPostContent(e.target.value);
-            }}
-            placeholder="Write a new post!"
-          />
-
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-      <div className="feed">
-        {posts.map((post, i) => (
-          <div key={post.id} className="post">
-            <div className="header">
-              <img src={post.user.avatar} />
-
-              <h2>{post.user.username}</h2>
-            </div>
-
-            <p className="content">{post.text}</p>
-          </div>
-        ))}
-      </div>
+      <Feed />
     </div>
   );
 };
---
+++ src/client/Feed.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { faker } from "@faker-js/faker";
+import "../../assets/css/style.css";
+
+const initialPosts = [1, 2].map((id) => ({
+  id,
+  text: faker.lorem.paragraph(),
+  user: { avatar: faker.image.avatar(), username: faker.internet.userName() },
+}));
+
+const Feed = () => {
+  const [posts, setPosts] = useState(initialPosts);
+  const [postContent, setPostContent] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newPost = {
+      id: posts.length + 1,
+      text: postContent,
+      user: {
+        avatar: faker.image.avatar(),
+        username: faker.internet.userName(),
+      },
+    };
+    console.log("postContent", postContent);
+
+    setPosts([newPost, ...posts]);
+
+    setPostContent("");
+  };
+  return (
+    <div className="container">
+      <div className="postForm">
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={postContent}
+            onChange={(e) => {
+              console.log("change");
+              setPostContent(e.target.value);
+            }}
+            placeholder="Write a new post!"
+          />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div className="feed">
+        {posts.map((post, i) => (
+          <div key={post.id} className="post">
+            <div className="header">
+              <img src={post.user.avatar} />
+
+              <h2>{post.user.username}</h2>
+            </div>
+
+            <p className="content">{post.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
