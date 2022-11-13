a7bde307fdbcf289b9b234f02b63fd16507856db
Elena Rakhimova <elenara@kth.se>
2022-11-13 12:47:55 +0100

    fix: add apollo client wrapper to styleguide config

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,5 +3,8 @@ const path = require("path");
 module.exports = {
   components: "src/client/components/**/*.js",
   require: [path.join(__dirname, "assets/css/style.css")],
+  styleguideComponents: {
+    Wrapper: path.join(__dirname, "src/client/styleguide/Wrapper"),
+  },
   webpackConfig: require("./webpack.client.config"),
 };
